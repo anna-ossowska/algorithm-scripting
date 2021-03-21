f556b9236b675d2176720e6a6a68808c7d7ffe7b
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-21 16:51:07 +0100

    Algorithm using the HashSet<T>

---
+++ Warm-up/SockMerchant/SockMerchant/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace SockMerchant
+{
+    class Program
+    {
+        static int sockMerchant(int n, int[] ar)
+        {
+            HashSet<int> socksHash = new HashSet<int>();
+            int sockPairs = 0;
+
+            foreach (var item in ar)
+            {
+                if (socksHash.Contains(item))
+                {
+                    socksHash.Remove(item);
+                    sockPairs++;
+                }
+                else
+                {
+                    socksHash.Add(item);
+                }
+            }
+            return sockPairs;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = Console.ReadLine().Trim().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            int result = sockMerchant(n, ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}

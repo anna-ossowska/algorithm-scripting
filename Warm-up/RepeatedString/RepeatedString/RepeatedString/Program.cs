d5eb15232d4312286754e13f22b7194287275b81
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-27 12:34:18 +0200

    Repeated String

---
+++ Warm-up/RepeatedString/RepeatedString/RepeatedString/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        public static long RepeatedString(string s, long n)
+        {
+            // aba --> 2
+            long counter = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            // aba aba aba --> 6
+            long multiply = n / s.Length;
+            counter *= multiply;
+
+
+            // aba aba aba a --> 7
+            long remainder = n % s.Length;
+            for (int i = 0; i < remainder; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            var result1 = RepeatedString("aba", 10);
+            var result2 = RepeatedString("a", 1000000000000);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
